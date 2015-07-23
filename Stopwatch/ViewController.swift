6a7a423dfbd2906b3383d77e25790c97b62bfc75
Vedran Stankovic <vedran@dynamicgroup.se>
2015-07-23 10:06:27 +0200

    added xcuserdata

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -9,7 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    var counter = 0
+    var isActive = false
+    
+    func updateTime() {
 
+        counter++
+        time.text = String(counter)
+
+    }
+    
+
+    @IBOutlet var time: UILabel!
+
+    @IBAction func stop(sender: AnyObject) {
+        
+        timer.invalidate()
+        counter = 0
+        time.text = "0"
+        isActive = false
+        
+    }
+    @IBAction func play(sender: AnyObject) {
+        
+        if isActive == false {
+            
+            isActive = true
+        
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
+        }
+    }
+    @IBAction func pause(sender: AnyObject) {
+        
+        timer.invalidate()
+        isActive = false
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
